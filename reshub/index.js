dba550f9b768381526ac89a58552aaba74e4afda
BROKY <catur@windowslive.com>
2021-03-13 19:41:49 +0700

    nodemon dan connect mongodb, bodyParser

--- reshub/index.js
+++ reshub/index.js
@@ -1,6 +1,11 @@
 // Import Express
-
 let Express = require("express")
+
+// Import Body Parser
+let bodyParser  = require("body-parser");
+
+// Import Mongoose
+let mongoose = require("mongoose");
 
 // Initialize App
 let app = Express()
@@ -12,10 +17,21 @@ let apiRouter = require("./api-routes")
 var port = process.env.PORT || 8080
 
 // Send Message for default URL
-app.get('/', (req, res) => res.send("Hello Word expressJS"));
+app.get('/', (req, res) => res.send("Hello Word expressJS and Nodemon"));
 
 // App apiRoutes 
 app.use("/api", apiRouter);
+
+// Config bodyparser
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+app.use(bodyParser.json());
+
+// Connect to Databses Mongoose
+mongoose.connect("mongodb://localhost/reshub");
+
+let db = mongoose.connection;
 
 // Launch App | Listen
 app.listen(port, function(){
